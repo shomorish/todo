90fe984fde468ca6b122f4a97184e2d8f3bba28f
shomorish <123480685+shomorish@users.noreply.github.com>
2024-04-20 16:06:16 +0900

    refactor: add custom dark and light themes.

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  colorScheme: ColorScheme.dark(),
+);
---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: ColorScheme.light(),
+);
