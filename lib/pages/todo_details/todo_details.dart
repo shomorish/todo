44d82498ccf2ee4096216d12f12cebe3fd61c507
shomorish <123480685+shomorish@users.noreply.github.com>
2024-04-22 18:11:51 +0900

    feat: implemented todo details page design.

--- lib/pages/todo_details/todo_details.dart
+++ lib/pages/todo_details/todo_details.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:todo/models/note/todo.dart';
 
 class ToDoDetailsPage extends StatelessWidget {
-  const ToDoDetailsPage({super.key});
+  final ToDo? toDo;
+  final TextEditingController titleController;
+  final TextEditingController detailsController;
+
+  ToDoDetailsPage({
+    super.key,
+    this.toDo,
+  })  : titleController = TextEditingController(text: toDo?.title ?? ''),
+        detailsController = TextEditingController(text: toDo?.details ?? '');
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
+    return Material(
+      child: ListView(
         children: [
-          TextField(),
-          TextField(),
+          // Title text field.
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Title',
+            ),
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          // Details text field.
+          TextField(
+            controller: detailsController,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Details',
+            ),
+            maxLines: null,
+          ),
         ],
       ),
     );
